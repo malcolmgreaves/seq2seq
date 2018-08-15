9cb3990c7d28713b6d5c047f58c952e31a560013
Malcolm Greaves <greaves.malcolm@gmail.com>
2018-08-15 14:37:45 -0700

    Init -- seq2seq playground

---
+++ README.md
@@ -0,0 +1,3 @@
+# seq2seq
+
+Playground for learning about sequence 2 sequence models. Specifically the encoder-decoder framework.
---
+++ tutorial_encode_decode.py
@@ -0,0 +1,5 @@
+# FROM:
+# https://machinelearningmastery.com/define-encoder-decoder-sequence-sequence-model-neural-machine-translation-keras/
+
+from typing import *
+
